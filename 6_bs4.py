15683baa3f9edf0fbf543d35ddb1241188703674
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-09-09 19:41:26 +0900

    Add files via upload

--- 6_bs4.py
+++ 6_bs4.py
@@ -18,6 +18,28 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # print(soup.a["href"]) # a element 의 속성  '값' 정보를 출력
 
-print(soup.find("a" , attrs={"class" : "Nbtn_upload"})) # class= "Nbtn_upload" 인 첫번째 a element 를 찾아줘
+# print(soup.find("a" , attrs={"class" : "Nbtn_upload"})) # class= "Nbtn_upload" 인 첫번째 a element 를 찾아줘
+# print(soup.find("li" , attrs={"class" : "rank01"}))
+# rank1 = soup.find("li" , attrs ={"class" : "rank01"})
+# print(rank1.a.get_text())
+# print(rank1.next_sibling)
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
 
-print(soup.find("li" , attrs={"class" : "rank01"}))
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+# print(rank1.parent)
+# rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+# rank2 = rank3.find_previous_sibling("li")
+# print(rank2.a.get_text())
+
+# print (rank1.find_next_siblings("li"))
+
+
+webtoon = soup.find("a" , text="외모지상주의-408화 1세대 왕 [05]")
+
+print(webtoon)
